d27e0b4f8cb69728e056c4632511f8395a0910e6
Krystian <zaba37@gmail.com>
2020-06-19 22:30:06 +0200

    Initial Commit

---
+++ CitySampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CitySampleApp
+//
+//  Created by Krystian Zabicki on 19/06/2020.
+//  Copyright © 2020 kzdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
